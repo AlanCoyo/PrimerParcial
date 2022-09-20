0d96039e11a9221ca5a507b5f976645c8518fd99
AlanCoyo <0238099@up.edu.mx>
2022-09-19 22:59:04 -0500

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const[nombre ,setName]=useState();
+  const[apellido ,setApellido]=useState();
+  const[apodo ,setApodo]=useState();
+
+  const isDisabledName = nombre!== undefined && nombre !== "";
+  const isDisabledApellido = apellido!== undefined && apellido !== "";
+  const isDisabledApodo = apodo!== undefined && apodo !== "";
+  
+const isDisableGlobal = isDisabledName && isDisabledApellido && isDisabledApodo;
+
+  const limpiar =()=>{
+    setName('');
+    setApellido('');
+    setApodo('');
+  }
+  const cambioNombre=(event)=>{
+    setName(event.target.value)
+  }
+  const cambioApellido=(event)=>{
+    setApellido(event.target.value)
+  }
+  const cambioApodo=(event)=>{
+    setApodo(event.target.value)
+  }
+  const enviar = (event) => {
+    event.preventDefault();
+    setFormEnv(true);
+  }
+  
+  const [formularioEnv, setFormEnv] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <div className='container'>
+    <div className ='App'>
+    <div className='cuadrado'>
+      <p>Nombre:</p>
+      <input onChange={cambioNombre} value={nombre}/>
+      </div>
+      <br/>
+      <div className='cuadrado'>
+      <p>Apellido:</p>
+      <input onChange={cambioApellido} value={apellido}/>
+      </div>
+      <br/>
+      <div className='cuadrado'>
+      <p>Apodo:</p>
+      <input onChange={cambioApodo} value={apodo}/>
+      </div>
+      
+      <br/>
+      <br/>
+      <br/>
+
+      <div className='botones'>
+      <button disabled = {!isDisableGlobal} onClick={enviar} >Enviar</button>
+      <button onClick = {limpiar}>Limpiar</button>
+      </div>
+      <br/>
+      <p>Información</p>
+      {formularioEnv && (<p> {`${nombre}`} {`${apellido}`} {`${apodo}`}</p>)}
+      
     </div>
+    </div>
+    </main>
   );
+  
 }
 
-export default App;
+
